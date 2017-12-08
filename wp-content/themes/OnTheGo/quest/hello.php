77d8681e77bc0cff1c36bde6ed68c9f4de100900
shkurik <oleksandr.shkuropat@gmail.com>
2017-12-09 00:46:27 +0200

    test .ignore rule commit

---
+++ wp-content/themes/OnTheGo/quest/hello.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>Тестируем PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Привет, мир! </p>'; ?>
+ <?php echo '<p>Привет! </p>'; ?>
+ <?php echo '<p>Привет! yes</p>'; ?>
+ <?php echo '<p>Привет! New master</p>'; ?>
+  <?php echo '<p>Привет! New Branch</p>'; ?>
+ </body>
+</html>
