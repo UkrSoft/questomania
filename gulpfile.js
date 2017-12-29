c61c9661050cf309af81f90d9e92a8f238293b9c
shkurik <oleksandr.shkuropat@gmail.com>
2017-12-29 14:01:16 +0200

    Сonnected NodeJS Unit tests to Travis. All tests run passed

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,8 @@
-'use strict';
-
 var gulp = require('gulp'),
-  mocha = require('gulp-mocha'),
-  gutil = require('gulp-util');
-  
+    mocha = require('gulp-mocha'),
+    gutil = require('gulp-util');
+
 gulp.task('mocha', function() {
-  return gulp.src(['./wp-content/plugins/quests-city/backend_nodejs/test/*.js'], {read:false})
-    .pipe(mocha({reporter: 'list'}))
-    .on('error', gutil.log);
+    return gulp.src(['./wp-content/plugins/quests-city/backend_nodejs/test/*.js'])
+        .pipe(mocha({reporter: 'list'}));
 });
-
-gulp.task('watch-mocha', function() {
-  gulp.run('mocha');
-  gulp.watch(['./wp-content/plugins/quests-city/backend_nodejs/**/*.js'], ['mocha']);
-});
-
-gulp.task('default', ['watch-mocha']);
